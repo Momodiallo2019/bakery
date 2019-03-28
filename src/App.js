6cf9aafd21275afba00ab2a622c60a6be1949fd8
Diallo Mamadou <Diallomohamed071@gmail.com>
2019-03-28 17:26:02 +0100

    première version

--- src/App.js
+++ src/App.js
@@ -1,28 +1,64 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Add from './component/Add';
+import List from './component/List';
+import Slider from "rc-slider";
+import Button from './component/core/Button';
+import 'rc-slider/assets/index.css';
+
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeTab: "add",
+      item: [],
+      price: 0
+    };
+    // this.onAddItem = this.onAddItem.bind(this);
+  }
+
+  onClickTabAdd = () => {
+    this.setState({
+      activeTab: "add"
+    })
+  }
+
+  onClickTabList = () => {
+    this.setState({
+      activeTab: "list"
+    })
+  }
+
+  onClickTabPay = () => {
+    this.setState({
+      activeTab: "pay"
+    })
+  }
+  
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+         <div>
+           <h1>Bakery </h1>
+           <div>
+           <Button onClick={this.onClickTabAdd}>Add</Button>
+           <Button onClick={this.onClickTabList}>List</Button>
+           <Button onClick={this.onClickTabPay}>Pay</Button>
+             {/* <Slider />  */}
+             </div>
+             <div>
+             {this.state.activeTab === 'add'
+             <Add.onClick= {this.onAddItem}; />
+              }
+             </div>
       </div>
     );
   }
 }
 
 export default App;
+
+
+
+
---
+++ src/component/Pay.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Slider from "rc-slider";
+import 'rc-slider/assets/index.css';
+
+class Pay extends Component {
+
+    render() {
+        return (
+        <div>
+            <h1>Pay</h1>        
+        </div>
+
+        );
+      }
+    }
+    
+    export default Pay;
+
+
+
